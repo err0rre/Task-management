693bdc42637526727c5bb97de0f239d3c1d0140e
err0rre <140460423+err0rre@users.noreply.github.com>
2024-10-18 00:01:55 +1000

    before actual deployment

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,20 @@
 //backend/db.js
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
 // Create a Sequelize instance and connect to the PostgreSQL database
 // Replace 'task_manager' with the name of the database you created, 'tailor' and 'password' are your database username and password.
-const sequelize = new Sequelize('task_manager', 'tailor', 'password', {
-  host: 'localhost',
-  dialect: 'postgres',
-  logging: console.log  // Ensure that SQL statements are logged to the console
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,       // 数据库名称
+  process.env.DB_USER,       // 数据库用户名
+  process.env.DB_PASSWORD,   // 数据库密码
+  {
+    host: process.env.DB_HOST,  // 数据库主机，在 docker-compose 中是 "db"
+    dialect: 'postgres',
+    port: process.env.DB_PORT,   // 数据库端口
+    logging: console.log
+  }
+);
 
 // Test the database connection to ensure Sequelize can connect to PostgreSQL successfully
 sequelize.authenticate()
@@ -19,4 +26,3 @@ sequelize.authenticate()
   });
 
 module.exports = sequelize;
-
